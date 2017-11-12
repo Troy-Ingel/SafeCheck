02c89a03414565dcc71f109f2976b75ae2eb2d63
Troy <troy.ing1996@gmail.com>
2017-11-12 12:00:02 -0500

    Added markers to mpa from people in database.

--- app/controllers/homeController.js
+++ app/controllers/homeController.js
@@ -36,12 +36,36 @@ function homeController($scope, GeoLocationFactory, GoogleMapsFactory, CheckInFa
 		});
 
 		CheckInFactory.getPeople().then(function(response){
-			console.log(response)
+			let people = response;
+
+			for(let i = 0; i < people.length; i++){
+				if(people[i]){
+					let position = {
+						lat: people.lat,
+						lng: people.lon
+					};
+
+					let status = people.status;
+					let name = people.first_name + ' ' + people.last_name;
+					GoogleMapsFactory.addMarker(position, map, name, '', getMarkerColor(status));
+				}
+			}
 		})
 	}
 	function getDirections(){
 		GoogleMapsFactory.getWalkingDirections('210 glenn drive stratford', 'boston ma').then(function(response){
 			console.log(response);
 		});
+	}
+
+	function() getMarkerColor(status){
+		if(status == 'G'){
+			return '../../img/GoogleMapsMarkers/green_MarkerS';
+		}
+		else if(status == 'I'){
+			return '../../img/GoogleMapsMarkers/yellow_MarkerA';
+		}else{
+			return '../../img/GoogleMapsMarkers/red_MarkerA';
+		}
 	}
 }
