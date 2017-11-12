def4c3ce609097e7b5cfa93158f0dfe9400e6dd0
Troy <troy.ing1996@gmail.com>
2017-11-12 12:59:38 -0500

    Add interval for updating map markers.

--- app/controllers/homeController.js
+++ app/controllers/homeController.js
@@ -2,13 +2,16 @@ angular
 .module('mainApp')
 .controller('homeController', homeController);
 
-homeController.$inject = ['$location', '$scope', 'GeoLocationFactory', 'GoogleMapsFactory', 'CheckInFactory'];
+homeController.$inject = ['$location', '$scope', '$interval', 'GeoLocationFactory', 'GoogleMapsFactory', 'CheckInFactory'];
 
-function homeController($location, $scope, GeoLocationFactory, GoogleMapsFactory, CheckInFactory){
+function homeController($location, $scope, $interval, GeoLocationFactory, GoogleMapsFactory, CheckInFactory){
 	
 	var map = undefined;
+	var markers = [];
 
 	$scope.getCurrentPage = getCurrentPage;
+
+	$interval(updateMarkers, 5000);
 
 	activate();
 
@@ -16,6 +19,7 @@ function homeController($location, $scope, GeoLocationFactory, GoogleMapsFactory
 
 	function activate(){
 		initMap();
+		reStyleMarkers();
 	}
 	function initMap() {
 		$scope.loading = true;
@@ -37,41 +41,7 @@ function homeController($location, $scope, GeoLocationFactory, GoogleMapsFactory
 			});
 
 			// GoogleMapsFactory.addMarker(myLatlng, map, 'Title', 'Me');
-
-
-			CheckInFactory.getPeople().then(function(response){
-
-				let people = response;
-
-				for(let i = 0; i < people.length; i++){
-					if(people[i]){
-
-						let curPerson = people[i];
-
-						let position = {
-							lat: parseFloat(curPerson.lat),
-							lng: parseFloat(curPerson.lon)
-						};
-
-						let initials = (curPerson.first_name.substring(0,1) + curPerson.last_name.substring(0,1)).toUpperCase();
-						
-						let status = curPerson.status;
-						let name = curPerson.first_name + ' ' + curPerson.last_name;
-						GoogleMapsFactory.addMarker(position, map, name, initials, getMarkerColor(status));
-					}
-				}
-
-				var markerElemQry = 'div[style*="height: 100px; margin-top: -50px; margin-left: -50%; display: table; border-spacing: 0px;"]';
-
-				var markerElements = $(markerElemQry);
-
-				console.log(markerElements)
-				markerElements.each(function(i, e){
-
-					console.log(e);
-					$(e).css('margin-top', '-55px');
-				});
-			});
+			loadMarkers();
 		});
 	}
 
@@ -94,5 +64,56 @@ function homeController($location, $scope, GeoLocationFactory, GoogleMapsFactory
 	
 	function getCurrentPage(){
 		return $location.path();
+	}
+
+	function loadMarkers(){
+		CheckInFactory.getPeople().then(function(response){
+
+			people = response;
+
+			for(let i = 0; i < people.length; i++){
+				if(people[i]){
+
+					let curPerson = people[i];
+
+					let position = {
+						lat: parseFloat(curPerson.lat),
+						lng: parseFloat(curPerson.lon)
+					};
+
+					let initials = (curPerson.first_name.substring(0,1) + curPerson.last_name.substring(0,1)).toUpperCase();
+
+					let status = curPerson.status;
+					let name = curPerson.first_name + ' ' + curPerson.last_name;
+					markers.push(GoogleMapsFactory.addMarker(position, map, name, initials, getMarkerColor(status)));
+				}
+			}
+		});
+	}
+
+	function updateMarkers(){
+		clearMarkers();
+		loadMarkers():
+	}
+
+	function clearMarkers(){
+		for (var i = 0; i < markers.length; i++) {
+			markers[i].setMap(null);
+		}
+
+		markers = [];
+	}
+
+	function reStyleMarkers(){
+		var markerElemQry = 'div[style*="height: 100px; margin-top: -50px; margin-left: -50%; display: table; border-spacing: 0px;"]';
+
+		var markerElements = $(markerElemQry);
+
+		console.log(markerElements)
+		markerElements.each(function(i, e){
+
+			console.log(e);
+			$(e).css('margin-top', '-55px');
+		});
 	}
 }
